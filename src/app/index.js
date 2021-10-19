1863de5cacd6fba22110f94f778848607064a0b2
Victor <victorgvargas>
2021-10-19 10:21:52 -0300

    Created db config

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const db = require("./models");
+
+db.sequelize.sync();
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to notes app" });
+});
 
 app.listen(port, () => {
   console.log("App running on port " + port);
